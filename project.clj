2a054c168d060d6515049e149de48ff8f5d96e63
Atamert Ölçgen <muhuk@muhuk.com>
2016-07-03 15:20:45 +0300

    Upgrade Clojure & Ring

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[com.cognitect/transit-clj "0.8.285" :exclusions [commons-codec]]
-                 [org.clojure/clojure "1.7.0"]
-                 [ring/ring-core "1.4.0"]]
+                 [org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.5.0"]]
   :plugins [[codox "0.8.15"]
             [com.jakemccrary/lein-test-refresh "0.10.0"]
             [jonase/eastwood "0.2.1"]]
   :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-beta1"]]}}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
   :codox {:defaults {:doc/format :markdown}
           :src-dir-uri "http://github.com/muhuk/ring-transit-middleware/blob/master/"
           :src-linenum-anchor-prefix "L"
           :output-dir "doc"}
-  :aliases {"all" ["with-profile" "dev:dev,1.6:dev,1.8"]})
+  :aliases {"all" ["with-profile" "dev:dev,1.6:dev,1.7"]})
