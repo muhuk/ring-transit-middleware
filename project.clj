0341199e02744c595098f4e2ff830223f5b5f204
Atamert Ölçgen <muhuk@muhuk.com>
2016-07-03 15:48:53 +0300

    Increment version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-transit-middleware "0.1.3-SNAPSHOT"
+(defproject ring-transit-middleware "0.1.4-SNAPSHOT"
   :description "Ring middleware for Transit request & responses."
   :url "https://github.com/muhuk/ring-transit-middleware"
   :license {:name "The MIT License"
